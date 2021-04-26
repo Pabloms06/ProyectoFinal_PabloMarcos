0d3fde744f4daa9e353f8756405a94f17d028e09
pablo <pablomarcossanchez06.pm@gmail.com>
2021-04-26 13:55:58 +0200

    Actualizado

--- app/src/main/java/com/example/proyectofinal_pablomarcos/MusicModel.kt
+++ app/src/main/java/com/example/proyectofinal_pablomarcos/MusicModel.kt
@@ -1,13 +1,21 @@
-package com.example.proyectofinal_pablomarcos
+package com.cbellmont.ejemplodescargainternet
 
-import android.widget.ImageView
+import com.google.gson.annotations.SerializedName
 
 data class MusicModel(
+    var title: String,
+    @SerializedName("id")var Id : String,
+    @SerializedName("decada")var decada: Int,
+    @SerializedName("genero") val genero : String,
+    @SerializedName("banda")  var banda : String,
+    @SerializedName("foto") var foto : String,
+    @SerializedName("descripcion") var descripcion : String,
+    @SerializedName("anio") var anio : String,
+    @SerializedName("cancion") var cancion : String,
+    @SerializedName("integrantes")var integrantes: List<String>?
+{
 
-    val icon : ImageView,
-    val desc : String,
-    val anio : String,
-    val cancion: String,
-    val integrantes: String
-
-)
+    override fun toString(): String {
+        return ""
+    }
+}
